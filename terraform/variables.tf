c59e38709bb7a71839826fcddbd19a29d72581c3
keerthisudha543grandhi <142565643+keerthisudha543grandhi@users.noreply.github.com>
2026-02-11 20:52:36 +0800

    Move Terraform EKS configuration to root terraform directory

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "ap-southeast-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "govtech-eks-dev"
+}
